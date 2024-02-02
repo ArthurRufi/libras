384e569c60514ec46ee83472142076d04a599c00
ArthurRufi <temparthur87@gmail.com>
2024-02-02 16:22:12 -0300

    inicada a API para consultar as informacoes, ainda nao fooi salvo no migrate

--- libras/main/models.py
+++ libras/main/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Cursos(models.Model):
-    ...
+    nome = models.CharField(max_length=200) 
+    logocurso = models.BinaryField()#adicionar aqui o local do upload, sair do binario
+    descricao = models.CharField(max_length=400)
