9a1e01b0f81011d56248e9a4a0f1853304afd554
ArthurRufi <temparthur87@gmail.com>
2024-02-08 23:25:16 -0300

    apicursos criada

---
+++ libras/apicursos/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import adicionar_curso, editar_nome_curso,excluir_curso, listar_cursos, n
+
+
+app_name = 'apicursos'
+
+
+urlpatterns = [
+]
