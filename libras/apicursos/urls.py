2f20d9e583510152a9b766c0f152e3ddaeb44af1
ArthurRufi <temparthur87@gmail.com>
2024-02-08 23:31:04 -0300

    apicursos http respondendo

--- libras/apicursos/urls.py
+++ libras/apicursos/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import adicionar_curso, editar_nome_curso,excluir_curso, listar_cursos, n
+from .views import apichamar
 
 
 app_name = 'apicursos'
 
 
 urlpatterns = [
+    path('', apichamar, name='chamar')
+
 ]
