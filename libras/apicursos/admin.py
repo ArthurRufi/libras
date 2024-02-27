73f767455200c96bf51e5f088958c2abe84259ab
ArthurRufi <temparthur87@gmail.com>
2024-02-27 13:17:22 -0300

    models da api gerenciadora de sinais

--- libras/apicursos/admin.py
+++ libras/apicursos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from main.models import Cursos, VideosCurso
+from .models import SinalsArchives
 # Register your models here.
 
 admin.site.register(Cursos)
 admin.site.register(VideosCurso)
+admin.site.register(SinalsArchives)
--- libras/apicursos/models.py
+++ libras/apicursos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SinalsArchives(models.Model):
+    
+    nome = models.CharField (max_length =255)
+    imagem = models.IntegerField()
+    linkvideo = models.CharField(max_length = 1000)
+    descricaoptbr = models.CharField(max_length = 2000)
+    descricaolibras = models.CharField(max_length= 1000)
