1902647097ea9ea836f5fa672e230fe77460a3e6
ArthurRufi <temparthur87@gmail.com>
2024-02-28 15:24:08 -0300

    ango.html agora retorna somente o codigo pertecente ao curso]

--- libras/main/urls.py
+++ libras/main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import adicionar_curso, editar_nome_curso,excluir_curso, listar_cursos, n, link, tutotial
+from .views import n, link, tutotial, sinais
 
 
 app_name = 'main'
@@ -8,14 +8,9 @@ app_name = 'main'
 
 urlpatterns = [
     path('', n, name='main'),
-    path('link/<int:cod>', link, name='linkreturn'),
-    path('tutorial/', tutotial, name = 'tutorial')
+    path('curso/<int:cod>', link, name='linkreturn'),
+    path('tutorial/', tutotial, name = 'tutorial'),
+    path('curso/<int:curso>/<int:sinal>', sinais)#    path('adicionar_curso/', adicionar_curso, name='adicionar_curso'),
 
-#    path('adicionar_curso/', adicionar_curso, name='adicionar_curso'),
- #   path('excluir_curso/<int:codigo_curso>/', excluir_curso, name='excluir_curso'),
-  #  path('editar_nome_curso/<int:codigo_curso>/', editar_nome_curso, name='editar_nome_curso'),
-   # path('listar_cursos/', listar_cursos, name='listar_cursos'),
-    
-    # Adicione outras URLs conforme necessário
      
 ]
--- libras/main/views.py
+++ libras/main/views.py
@@ -12,46 +12,24 @@ def n(request):
 def link(request, cod):
     try:
         redirection = VideosCurso.objects.get(codigocurso = cod)
+        codigo = cod
         cursoname = Cursos.objects.get(codigocurso = cod)
-        link_do_video = redirection.link  
-        sinal = SinalsArchives.objects.values('nome', 'descricaolibras', 'descricaolibras', 'linkvideo', 'imagem')
+        link_do_video = redirection.link
+        sinal = SinalsArchives.objects.values('nome', 'descricaolibras', 'descricaolibras', 'linkvideo', 'imagem', 'codigocurso')
         
-        return render(request, 'main/html/anjo.html', {'link_do_video': link_do_video, 'nome': cursoname, 'sinal': sinal})
+        
+        return render(request, 'main/html/anjo.html', {'link_do_video': link_do_video, 'nome': cursoname, 'sinal': sinal, 'codigo': codigo})
     
     except redirection.DoesNotExist:
         return HttpResponse('INNASNDN')
 
-def adicionar_curso(request):
-    if request.method == 'POST':
-        nome = request.POST.get('nome')
-        codigo = int(request.POST.get('codigo'))
 
-        Cursos.adicionar_curso(nome=nome, codigo=codigo) 
-        return HttpResponse("Curso adicionado com sucesso!")
-
-    return render(request, 'sua_template.html')  
-
-
-def excluir_curso(request, codigo_curso):
-    Cursos.excluir_curso(codigo=codigo_curso)
-    return HttpResponse("Curso excluído com sucesso!")
-
-
-def editar_nome_curso(request, codigo_curso):
-    if request.method == 'POST':
-        novo_nome = request.POST.get('novo_nome')
-
-        curso = get_object_or_404(Cursos, codigocurso=codigo_curso)
-        curso.editar_nome_curso(novo_nome=novo_nome)
-
-        return HttpResponse("Nome do curso editado com sucesso!")
-
-    return render(request, 'sua_template.html') 
-
-
-def listar_cursos(request):
-    cursos = Cursos.objects.all()
-    return render(request, 'listar_cursos.html', {'cursos': cursos})
+def sinais(request, curso, sinal):
+    try:
+        redirection = SinalsArchives.objects.all()
+        return render(request, 'main/html/details.html')
+    except redirection.DoesNotExist:
+        return HttpResponse('agua mineral')
 
 
 def tutotial(request):
