0b5491cae2977a0cbda8ba16d921e253b6da0425
ArthurRufi <temparthur87@gmail.com>
2024-03-22 11:10:14 -0300

    integração das API modelos REST para o projeto

--- libras/apicursos/views.py
+++ libras/apicursos/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .serializers import GroupSerializer, UserSerializer
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+    
 def apichamar(request):
     return HttpResponse('OK')
--- libras/main/urls.py
+++ libras/main/urls.py
@@ -3,14 +3,15 @@ from django.urls import path, include
 from .views import n, link, tutotial, sinais
 
 
+
 app_name = 'main'
 
 
 urlpatterns = [
     path('', n, name='main'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('curso/<int:cod>', link, name='linkreturn'),
     path('tutorial/', tutotial, name = 'tutorial'),
-    path('curso/<int:curso>/<int:sinal>', sinais)#    path('adicionar_curso/', adicionar_curso, name='adicionar_curso'),
-
+    path('curso/<int:curso>/<int:sinal>', sinais),#    path('adicionar_curso/', adicionar_curso, name='adicionar_curso'),
      
 ]
